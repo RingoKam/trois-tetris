8d3f28a447fefa756c30a51fe86e8d2eee91ad3f
ringo kam <ringo_kam@hotmail.com>
2021-12-24 13:49:57 -0500

    init

---
+++ src/components/Block.vue
@@ -0,0 +1,58 @@
+<template>
+    
+</template>
+
+<script lang="ts">
+import { BoxGeometry, Mesh } from "troisjs";
+
+const SHAPES = [
+  [
+    { x: 0, y: 0, z: 0 },
+    { x: 1, y: 0, z: 0 },
+    { x: 1, y: 1, z: 0 },
+    { x: 1, y: 2, z: 0 },
+  ],
+  [
+    { x: 0, y: 0, z: 0 },
+    { x: 0, y: 1, z: 0 },
+    { x: 0, y: 2, z: 0 },
+  ],
+  [
+    { x: 0, y: 0, z: 0 },
+    { x: 0, y: 1, z: 0 },
+    { x: 1, y: 0, z: 0 },
+    { x: 1, y: 1, z: 0 },
+  ],
+  [
+    { x: 0, y: 0, z: 0 },
+    { x: 0, y: 1, z: 0 },
+    { x: 0, y: 2, z: 0 },
+    { x: 1, y: 1, z: 0 },
+  ],
+  [
+    { x: 0, y: 0, z: 0 },
+    { x: 0, y: 1, z: 0 },
+    { x: 1, y: 1, z: 0 },
+    { x: 1, y: 2, z: 0 },
+  ],
+];
+
+export default {
+  props: {
+    BlockType: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    const geometry = new BoxGeometry(1, 1, 1);
+    const mesh = new Mesh({});
+    return {
+      mesh,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
